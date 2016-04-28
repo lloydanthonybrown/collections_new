020acc121db5afa6d9ec8a6d8a22e4ec6142eac5
lloydanthonybrown <lloydanthonybrown@gmail.com>
2016-04-28 12:10:54 -0600

    Added Happy path for a descending Iterator

--- src/SandboxingTreeSet.java
+++ src/SandboxingTreeSet.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.TreeSet;
 
 public class SandboxingTreeSet {
@@ -43,10 +44,14 @@ public class SandboxingTreeSet {
             System.out.println("The comparator I'm using is: " + multipleCharTypeTreeSet.comparator());
 
 // Boolean contains
-        System.out.println("Does my TreeSet contain bacon? (contains) " + multipleCharTypeTreeSet.contains("bacon"));
+        System.out.println("Does my TreeSet contain bacon? " + multipleCharTypeTreeSet.contains("bacon"));
 
 // Descending Iterator
-        System.out.println("A Descending Iterator of my TreeSet: " + multipleCharTypeTreeSet.descendingIterator());
+        Iterator mySadIterator;
+        mySadIterator = multipleCharTypeTreeSet.descendingIterator();
+        System.out.println("My TreeSet values: ");
+        while (mySadIterator.hasNext())
+            System.out.printf(mySadIterator.next() + " "); //Why does this need a string?
 
 // first
         System.out.println("Results of the 'first' method: " + multipleCharTypeTreeSet.first());
@@ -67,22 +72,34 @@ public class SandboxingTreeSet {
 // isEmpty
         System.out.println("Results of the 'isEmpty' method: " + multipleCharTypeTreeSet.isEmpty());
 // iterator
+        // This one still isn't working quite right
         System.out.println("Results of the 'iterator' method: " + multipleCharTypeTreeSet.iterator());
 // remove
         System.out.println("Contents of the TreeSet prior to removing Elmo: " + multipleCharTypeTreeSet);
-        System.out.println("Results of the 'remove' method: " + multipleCharTypeTreeSet.remove("Elmo"));
+        System.out.println("Results of the 'remove' method: " + multipleCharTypeTreeSet.remove("Elmo")); // I didn't realize this, but this is a boolean method.
         System.out.println("Contents of the TreeSet after removing Elmo: " + multipleCharTypeTreeSet);
 // size
         System.out.println("Results of the 'size' method: " + multipleCharTypeTreeSet.size());
 // subset
-        System.out.println("Results of the 'subset' method, using 64 and wascally wabbit: " + multipleCharTypeTreeSet.subSet("64","wascally wabbit"));
+        System.out.println("Results of the 'subset' method, using 64 and Cruella De Ville: " + multipleCharTypeTreeSet.subSet("64","Cruella De Ville"));
 // headset
-        System.out.println("Results of the 'last' method: " + multipleCharTypeTreeSet.headSet("Big Bird"));
+        System.out.println("Contents of the TreeSet for using the 'headset' and 'tailset' methods: " + multipleCharTypeTreeSet);
+        System.out.println("Results of the 'headset' method, using 'Big Bird': " + multipleCharTypeTreeSet.headSet("Big Bird"));
 // tailset
-        System.out.println("Results of the 'last' method: " + multipleCharTypeTreeSet.tailSet("1"));
+        System.out.println("Results of the 'tailset' method, using '1': " + multipleCharTypeTreeSet.tailSet("1"));
 
 // Nasty Paths
         System.out.println("NASTY PATHS");
+
+// Using floor method on a value that doesn't exist
+//
+//
+//
+// Using floor method on a value that doesn't exist
+// Using floor method on a value that doesn't exist
+// Using floor method on a value that doesn't exist
+// Using floor method on a value that doesn't exist
+// Using floor method on a value that doesn't exist
 
     }
 }
